a121187608253dd2b9db22ec279a9d6f90aacc51
sukant <techteen90@gmail.com>
2025-01-25 17:10:03 +0530

    package updates for production

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,9 @@ import cookieParser from 'cookie-parser'
 import authRoutes from './routes/auth.routes.js'
 import messageRoutes from './routes/message.routes.js'
 import { app } from './lib/socket.js'
+import path from 'path'
+
+const __dirname = path.resolve()
 
 
 app.use(express.json({ limit: "50mb" }));
@@ -18,6 +21,17 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, ".../frontend/chatit/dist")))
+
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, ".../frontend/chatit", "dist", "index.html"))
+
+    })
+
+}
 
 
 export default app
