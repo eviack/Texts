0b3228a45b270fdf83e6201601ceb19838ae9d73
sukant <techteen90@gmail.com>
2025-01-25 17:36:40 +0530

    main

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,11 +23,11 @@ app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, ".../frontend/chatit/dist")))
+    app.use(express.static(path.join(__dirname, "../frontend/chatit/dist")))
 
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, ".../frontend/chatit", "dist", "index.html"))
+        res.sendFile(path.join(__dirname, "../frontend/chatit/dist", "index.html"))
 
     })
 
